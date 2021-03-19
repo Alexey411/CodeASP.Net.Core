69b5008adc397067a8825896857a1b6e8ea7664a
Alexey411 <79913605+Alexey411@users.noreply.github.com>
2021-03-19 09:40:02 +0300

    Asp.Net core

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WA20.interfaces;
+using WA20.Models;
+using WA20.ViewModels;
+
+namespace WA12.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllRealty _allRealty;
+        private readonly IRealtyCategory _allCategories;
+
+        public CarsController(IAllRealty iAllRealty, IRealtyCategory iRealtyCat)
+        {
+            _allRealty = iAllRealty;
+            _allCategories = iRealtyCat;
+        }
+
+        public ViewResult Index()
+        {
+            ViewBag.Title = "Недвижимость";
+            RealtyListViewModel obj = new RealtyListViewModel();
+            obj.allRealty = _allRealty.Realty;
+            obj.currCategory = "Недвижимость";
+
+            return View(obj);
+        }
+    }
+}
